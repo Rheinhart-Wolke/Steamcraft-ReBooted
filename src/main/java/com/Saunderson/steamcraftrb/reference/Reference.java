d7d4fe677a0a94d93d7944cbe854bdf7a56494c4
linuxdemon1 <walter@walterbarnes.net>
2016-03-09 06:51:07 -0600

    Cleaned up user-specific files that should not exist in repo

--- src/main/java/com/Saunderson/steamcraftrb/reference/Reference.java
+++ src/main/java/com/Saunderson/steamcraftrb/reference/Reference.java
@@ -6,8 +6,8 @@ public class Reference
 {
     public static final String MOD_ID = "SteamcraftRB";
     public static final String MOD_NAME = "Steamcraft-ReBooted";
-    public static final String VERSION = "Alpha-1.4";
-    public static final String MCVERSION = "1.7.10";
+    public static final String VERSION = "@VERSION@";
+    public static final String MCVERSION = "@MCVERSION@";
     public static final String CLIENT_PROXY_CLASS = "com.Saunderson.steamcraftrb.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.Saunderson.steamcraftrb.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS = "com.Saunderson.steamcraftrb.client.gui.GuiFactory";
